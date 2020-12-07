d828f8a74b297c13cb322f83ef0fd417fa0c074c
312054439@qq.com <wy0223>
2020-12-07 16:07:23 +0800

    最长回文

---
+++ leetcode/src/main/java/com/wu/leetcode/leetcode/November/Palindrome.java
@@ -0,0 +1,143 @@
+package com.wu.leetcode.leetcode.November;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author wuxuyang
+ * @date 2020/11/12 15:13
+ */
+public class Palindrome {
+    /**
+     * 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+     * 示例 1：
+     * <p>
+     * 输入: "babad"
+     * 输出: "bab"
+     * 注意: "aba" 也是一个有效答案。
+     */
+    public String lPalindrome(String s) {
+        //假设 入参为 adasddsaasddddddds
+        //
+        int length = s.length();
+        String res = "";
+        for (int i = 0; i < length; i++) {
+            String zl = "";
+            String fl = "";
+            List<Character> list = new ArrayList<>();
+            zl = zl + s.charAt(i);
+            fl = s.charAt(i) + fl;
+
+        }
+        return res;
+    }
+
+    //正确答案
+    public String longestPalindrome(String s) {
+        // 特判
+        int len = s.length();
+        if (len < 2) {
+            return s;
+        }
+
+        int maxLen = 1;
+        int begin = 0;
+
+        // dp[i][j] 表示 s[i, j] 是否是回文串
+        boolean[][] dp = new boolean[len][len];
+        char[] charArray = s.toCharArray();
+
+        for (int i = 0; i < len; i++) {
+            dp[i][i] = true;
+        }
+        for (int j = 1; j < len; j++) {
+            for (int i = 0; i < j; i++) {
+                if (charArray[i] != charArray[j]) {
+                    dp[i][j] = false;
+                } else {
+                    if (j - i < 3) {
+                        dp[i][j] = true;
+                    } else {
+                        dp[i][j] = dp[i + 1][j - 1];
+                    }
+                }
+
+                // 只要 dp[i][j] == true 成立，就表示子串 s[i..j] 是回文，此时记录回文长度和起始位置
+                if (dp[i][j] && j - i + 1 > maxLen) {
+                    maxLen = j - i + 1;
+                    begin = i;
+                }
+            }
+        }
+        return s.substring(begin, begin + maxLen);
+    }
+
+    public String newLongestPalindrome(String s) {
+        int len = s.length();
+        if (len < 2) {
+            return s;
+        }
+        int maxLen = 1;
+        int begin = 0;
+        boolean[][] dp = new boolean[len][len];
+        char[] charArray = s.toCharArray();
+        for (int i = 0; i < len; i++) {
+            dp[i][i] = true;
+        }
+        for (int j = 1; j < len; j++) {
+            for (int i = 0; i < j; i++) {
+                if (charArray[i] != charArray[j]) {
+                    dp[i][j] = false;
+                }
+                if (j - i < 3) {
+                    dp[i][j] = true;
+                } else {
+                    dp[i][j] = dp[i + 1][j - 1];
+                }
+                if (dp[i][j] && j - i + 1 > maxLen) {
+                    maxLen = j - i + 1;
+                    begin = i;
+                }
+                return s.substring(begin, begin + maxLen);
+            }
+        }
+        return null;
+    }
+
+    public static String test(String s) {
+        int length = s.length();
+
+        if (length < 2) {
+            return s;
+        }
+
+        int maxLen = 1;
+        int begin = 0;
+
+        char[] charArray = s.toCharArray();
+        boolean[][] dp = new boolean[length][length];
+        for (int i = 0; i < length; i++) {
+            dp[i][i] = true;
+        }
+        for (int j = 1; j < length; j++) {
+            for (int i = 0; i < j; i++) {
+                if (charArray[i] != charArray[j]) {
+                    dp[i][j] = false;
+                }
+                if (j - i < 3) {
+                    dp[i][j] = true;
+                } else {
+                    dp[i][j] = dp[i + 1][j - 1];
+                }
+                if (dp[i][j] &&j-i+1>maxLen){
+                    maxLen=j-i+1;
+                    begin=i;
+                }
+            }
+        }
+        return s.substring(begin,begin+maxLen);
+    }
+
+}
