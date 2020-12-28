ef2e48f84123073eed4168ac25f99e2d64c0d5ba
312054439@qq.com <wy0223>
2020-12-28 17:15:36 +0800

    455. 分发饼干

---
+++ leetcode/src/main/java/com/wu/leetcode/leetcode/theYear2020/December/MaxProfit.java
@@ -0,0 +1,18 @@
+package com.wu.leetcode.leetcode.theYear2020.December;
+
+/**
+ * @author wuxuyang
+ * @date 2020/12/28 15:45
+ */
+public class MaxProfit {
+    /**
+     *给定一个整数数组 prices ，它的第 i 个元素 prices[i] 是一支给定的股票在第 i 天的价格。
+     *
+     * 设计一个算法来计算你所能获取的最大利润。你最多可以完成 k 笔交易。
+     *
+     * 注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
+     */
+    public int maxProfit(int k, int[] prices) {
+
+    }
+}
